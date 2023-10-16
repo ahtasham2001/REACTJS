f60144f7eac9cd19bddd73d6342d256f2c1d4f6c
AHTASHAM NAEEM <ahtashamse2001@gmail.com>
2023-10-16 13:25:45 +0500

    CSS CLEAN START TAILWIND

--- 07counterTailwind/src/App.jsx
+++ 07counterTailwind/src/App.jsx
@@ -5,9 +5,15 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     
-    </>
+     <div className='w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10'>
+      <div>Increment & Decrement</div>
+      <div>
+        <button>-</button>
+        <div></div>
+        <button>+</button>
+      </div>
+      <button>Reset</button>
+     </div>
   )
 }
 
