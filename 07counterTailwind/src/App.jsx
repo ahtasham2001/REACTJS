d2433084146f18e02a93c1b094fbdcfc530c88fa
AHTASHAM NAEEM <ahtashamse2001@gmail.com>
2023-10-16 13:37:57 +0500

    Half Project Done

--- 07counterTailwind/src/App.jsx
+++ 07counterTailwind/src/App.jsx
@@ -4,15 +4,22 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  function decreaseHandler(){
+    setCount(count-1)
+  }
+  function increaseHandler(){
+    setCount(count+1)
+  }
+
   return (
      <div className='w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10'>
-      <div>Increment & Decrement</div>
-      <div>
-        <button>-</button>
+      <div className='text-[#0398d4] font-medium text-2xl'>Increment & Decrement</div>
+      <div className='bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]'>
+        <button className='border-r-2 text-center w-20 border-[#bfbfbf] text-5xl' onClick={decreaseHandler}>-</button>
         <div></div>
-        <button>+</button>
+        <button className='border-l-2 text-center w-20 border-[#bfbfbf] text-5xl' onClick={increaseHandler}>+</button>
       </div>
-      <button>Reset</button>
+      <button className='bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg'>Reset</button>
      </div>
   )
 }
