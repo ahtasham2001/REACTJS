afaa21144c892e742e51589bc1d95b93cc8cb0c3
AHTASHAM NAEEM <ahtashamse2001@gmail.com>
2023-10-19 14:26:54 +0500

    UseEFFECT Variation Done

--- 09dummyProject/src/App.jsx
+++ 09dummyProject/src/App.jsx
@@ -1,8 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [text,setText] = useState("")
+
+// variation - 1 -> Every Render
+
+// useEffect(()=>{
+//   console.log("UI RENDERING DONE")
+// })
+
+// variaton - 2 -> First Render
+
+// useEffect(()=>{
+//   console.log("UI Rendering Done")
+// },[])
+
+// variation - 3 -> First Render + Wnenever dependency changes
+
+// useEffect(()=>{
+//   console.log("UI Rendering Done");
+// },[text])
+
+// variation - 4 -> to handle unmounting of a component
+useEffect(()=>{
+  console.log("Listener added")
+
+  return ()=>{
+    console.log("Listener Removed");
+  }
+},[text])
+
   function changeHandler(event){
     setText(event.target.value)
     console.log(text)
