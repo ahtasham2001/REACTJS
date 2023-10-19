b4a71818a8c7dfc7a77aea6025ba2044d1c07cfd
AHTASHAM NAEEM <ahtashamse2001@gmail.com>
2023-10-19 13:41:55 +0500

    APP .jsx Changes

--- 09dummyProject/src/App.jsx
+++ 09dummyProject/src/App.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [text,setText] = useState("")
+  function changeHandler(event){
+    setText(event.target.value)
+    console.log(text)
+  }
+
+
   return (
     <div className='App'>
-      <input type="text"></input>
+      <input type="text" onChange={changeHandler}></input>
     </div>
   )
 }
