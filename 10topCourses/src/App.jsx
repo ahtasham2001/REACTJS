b01a21199591d968228228f17bdcb8898cfb77b9
AHTASHAM NAEEM <ahtashamse2001@gmail.com>
2023-10-22 14:06:44 +0500

    All Component and Api Done

--- 10topCourses/src/App.jsx
+++ 10topCourses/src/App.jsx
@@ -1,12 +1,24 @@
 import './App.css'
+import Navbar from "./Components/Navbar"
+import Filter from "./Components/Filter"
+import Cards from "./Components/Cards"
 
 function App() {
-  
+
 
   return (
     <div>
-      App
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Filter />
+      </div>
+      <div>
+        <Cards />
+      </div>
     </div>
+
   )
 }
 
