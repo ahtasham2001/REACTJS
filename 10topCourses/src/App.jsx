db3c204577cc7d91c58bd3255d23c5d5dcd2142b
AHTASHAM NAEEM <ahtashamse2001@gmail.com>
2023-10-22 14:43:46 +0500

    Spinner Commponent Start

--- 10topCourses/src/App.jsx
+++ 10topCourses/src/App.jsx
@@ -2,20 +2,44 @@ import './App.css'
 import Navbar from "./Components/Navbar"
 import Filter from "./Components/Filter"
 import Cards from "./Components/Cards"
+import { apiUrl, filterData } from './data'
+import { useEffect, useState } from 'react'
+import Spinner from "./Components/Spinner"
+import {toast} from "react-toastify"
 
 function App() {
+const [courses, setCoourses] = useState(null);
+const [loading , setloading] = useState(true)
 
 
+async function fetchData() {
+  setloading(true);
+  try {
+    let response = await fetch(apiUrl);
+    let output = await response.json();
+    setCoourses(output); 
+  } catch (error) {
+    toast.error("Network Issue Brow")
+  }
+  setloading(false);
+  
+}
+
+useEffect(()=>{
+  fetchData();
+},[])
   return (
     <div>
       <div>
         <Navbar />
       </div>
       <div>
-        <Filter />
+        <Filter filterData= {filterData} />
       </div>
       <div>
-        <Cards />
+        {
+          loading ? (<Spinner/>) : (<Cards/>)
+        }
       </div>
     </div>
 
--- 10topCourses/src/Components/Filter.jsx
+++ 10topCourses/src/Components/Filter.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 
-const Filter = ()=>{
+const Filter = (props)=>{
+    let filterData = props.filterData;
     return(
         <div>
-
+            {
+                filterData.map((data)=>(
+                    <button>{data.title}</button>
+                ))
+            }
         </div>
     )
 }
