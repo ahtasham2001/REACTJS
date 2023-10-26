47c160beef4906fc96ac2d5822f21d1a008bf010
AHTASHAM NAEEM <ahtashamse2001@gmail.com>
2023-10-26 14:53:14 +0500

    Form React

--- formReact/src/App.jsx
+++ formReact/src/App.jsx
@@ -1,12 +1,38 @@
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  
+  const [formData , setformData] = useState({firstName: "", lastName: "", email: ""});
+  function changeHandler(event) {
+    setformData(preFormData =>{
+      return{
+        ...preFormData,
+        [event.target.name] : event.target.value
+      }
+    })
+  }
 
   return (
     <div>
       <form action="">
-        <input type="text" placeholder='First Name' />
+        <input 
+        type="text" 
+        placeholder='Enter Your First Name here'
+        onChange={changeHandler}
+         />
+         <br />
+         <br />
+        <input 
+        type="text" 
+        placeholder='Enter Your Last Name here'
+        onChange={changeHandler}
+         />
+         <br />
+         <br />
+        <input 
+        type="text" 
+        placeholder='Enter Your Email here'
+        onChange={changeHandler}
+         />
       </form>
     </div>
   )
