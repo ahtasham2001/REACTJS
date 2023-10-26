4db8c5bd9d61a2ff76dab6097810d31cc8254065
AHTASHAM NAEEM <ahtashamse2001@gmail.com>
2023-10-26 13:52:49 +0500

    Form React

---
+++ formReact/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <form action="">
+        <input type="text" placeholder='First Name' />
+      </form>
+    </div>
+  )
+}
+
+export default App
