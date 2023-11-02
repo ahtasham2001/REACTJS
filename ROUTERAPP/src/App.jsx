62ea7cb60957c16542f31aa6e2a9af40b67d0f80
AHTASHAM NAEEM <ahtashamse2001@gmail.com>
2023-11-02 15:03:52 +0500

    JS Advance

--- ROUTERAPP/src/App.jsx
+++ ROUTERAPP/src/App.jsx
@@ -4,7 +4,12 @@ function App() {
 
   return (
    <Routes>
-    <Route path="/" element={<Home/>} />
+    <Route path="/" element={<Home/>} >
+      <Route path="/about" element={<About/>} />
+      <Route path="/labs" element={<Labs/>} />
+      <Route path="/notfound" element={<NotFound/>} />
+      <Route path="/support" element={<Support/>} />
+    </Route>
    </Routes>
   )
 }
