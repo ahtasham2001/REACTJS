133bc4f1307670810a26f05998ca5f032ddefa0e
AHTASHAM NAEEM <ahtashamse2001@gmail.com>
2023-11-06 14:27:24 +0500

    Router Configuration Done

--- routerProject/src/App.jsx
+++ routerProject/src/App.jsx
@@ -1,9 +1,22 @@
-
+import { Routes,Route } from "react-router";
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import Sinup from "./pages/Sinup"
+import Login from "./pages/Login"
+import Dashboard from "./pages/Dashboard"
 function App() {
 
 
   return (
-  <div>Hwllow World</div>
+  <div>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="login" element={<Login/>} />
+      <Route path="sinup" element={<Sinup/>} />
+      <Route path="dashboard" element={<Dashboard/>} />
+    </Routes>
+  </div>
   )
 }
 
